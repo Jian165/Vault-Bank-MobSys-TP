97167df5730c75c7eafe3330e1aa5cc095fc3c23
Jian <duranjhonmark2@gmail.com>
2025-03-21 14:48:44 +0800

    add Login Page

--- app/src/main/java/com/example/vaultbank/MainActivity.java
+++ app/src/main/java/com/example/vaultbank/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vaultbank;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnGetStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        loadComponents();
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, Login.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+    }
+
+    private void loadComponents()
+    {
+        btnGetStarted = findViewById(R.id.btn_getStarted);
     }
 }
