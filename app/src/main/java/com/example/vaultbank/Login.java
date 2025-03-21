a2a36872d4f5ad547bf1b7ac0f037b84ed2c3ed5
Jian <duranjhonmark2@gmail.com>
2025-03-21 15:32:32 +0800

    Finish Login Page

--- app/src/main/java/com/example/vaultbank/Login.java
+++ app/src/main/java/com/example/vaultbank/Login.java
@@ -1,6 +1,11 @@
 package com.example.vaultbank;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    CheckBox cbVisibility;
+    EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,26 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        LaodComponents();
+
+        cbVisibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else {
+                    edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+
+    private void LaodComponents()
+    {
+        edtPassword = findViewById(R.id.edt_password);
+        cbVisibility = findViewById(R.id.cb_visibility);
+
     }
 }
